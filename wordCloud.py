d8dbc963d182da636a925624bf8ad17b8a37592b
voha <zi4.psy@gmail.com>
2022-08-31 01:34:09 +0300

    added removed symbols

--- wordCloud.py
+++ wordCloud.py
@@ -11,17 +11,24 @@ translator = Translator()
 # input_text = input("Введите текст :")
 
 
-d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-text2 = open(path.join(d, 'dataWord/VEED-subtitles_The2.txt')).read()
+d = path.dirname(__file__) \
+    if "__file__" in locals() \
+    else os.getcwd()
+subtitre = open(path.join(d, 'dataWord/VEED-subtitles_The3.txt')).read()
 
 # f = open('text.txt', 'r')
 # translator.detect(input_text)
-result = translator.translate(text2, dest='ru')
+resultTranslate = translator.translate(subtitre, dest='ru')
 
-text = [text2, result.text]
+text = [subtitre, resultTranslate.text]
 
-text1 = ' '.join(text)
-print(text1)
+textToString = ' '.join(text)
+
+chars = '.,!-?'
+
+removeSymbols = textToString.translate(str.maketrans(' ', ' ', chars))
+
+print(removeSymbols)
 
 x, y = np.ogrid[:300, :300]
 
@@ -29,7 +36,7 @@ mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
 mask = 255 * mask.astype(int)
 
 wc = WordCloud(background_color="white", repeat=True, mask=mask)
-wc.generate(text1)
+wc.generate(removeSymbols)
 
 plt.axis("off")
 plt.imshow(wc, interpolation="bilinear")
